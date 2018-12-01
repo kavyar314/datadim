1372f9ec0cafd4c7d70502bb1400bc4498ec7e40
Kavya Ravichandran <rkavya@mit.edu>
2018-12-01 17:35:27 -0500

    log option

--- make_plots.py
+++ make_plots.py
@@ -8,13 +8,19 @@ import os
 PATH_TO_SV = './singular_values/'
 
 OUT_PATH = './singular_values/plots/'
+
+LOG = True
+
 if 'plots' not in os.listdir('./singular_values'):
 	os.makedirs(OUT_PATH)
 
-def plot_singular_values(sv_array, specs):
+def plot_singular_values(sv_array, specs, log=LOG):
 	if sv_array[0] != 1:
 		sv_array/sv_array[0]
 	fname = specs + '.pdf'
+	if log:
+		sv_array = np.log(sv_array)
+		fname = 'log' + fname
 	plt.figure()
 	plt.plot(sv_array)
 	plt.savefig(os.path.join(OUT_PATH, fname))

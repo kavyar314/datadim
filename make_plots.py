1ce8aba8660e18bf96e839129fb2c5c4465f9683
Kavya Ravichandran <rkavya@mit.edu>
2018-12-06 08:48:32 -0500

    all classes on same plot, by layer

--- make_plots.py
+++ make_plots.py
@@ -7,7 +7,7 @@ import os
 
 PATH_TO_SV = './singular_values/'
 
-OUT_PATH = './singular_values/plots/'
+OUT_PATH = './singular_values/plots/on_same/'
 
 FAILED_FILES = './plot_failed_files.txt'
 
@@ -16,34 +16,51 @@ LOG = True
 if 'plots' not in os.listdir('./singular_values'):
 	os.makedirs(OUT_PATH)
 
-def plot_singular_values(sv_array, specs, log=LOG):
-	if sv_array[0] != 1:
-		sv_array/sv_array[0]
-	fname = specs + '.pdf'
-	if log:
-		sv_array = np.log(sv_array)
-		fname = 'log' + fname
+if 'on_same' not in os.listdir('./singular_values/plots'):
+	os.makedirs(OUT_PATH)
+
+def plot_singular_values(sv_arrays, specs, log=LOG):
+	'''
+	sv_arrays is a list of list of singular values, ordered by class (0-9)
+	need to 
+	'''
+	for sv_array in sv_arrays:
+		if sv_array[0] != 1:
+			sv_array/sv_array[0]
+		fname = specs + '.pdf'
+		if log:
+			sv_array = np.log(sv_array)
+			fname = 'log' + fname
 	plt.figure()
-	plt.plot(sv_array)
+	for sv_array in sv_arrays:
+		plt.plot(sv_array)
+	plt.legend(range(len(sv_arrays)))
 	plt.savefig(os.path.join(OUT_PATH, fname))
 	plt.close()
 
 def open_and_process_singular_values(fname):
+	# singularValues_cifar10_train_c3_activation_15.npy
+	# [0]_[1]model_[2]split_[3]class_[4]_[5]layer.npy
     #print fname
 	singular_vals = np.load(os.path.join(PATH_TO_SV + fname))
 	return singular_vals/singular_vals[0]
 
 if __name__ == '__main__':
 	sv_files = [f for f in os.listdir(PATH_TO_SV) if '.npy' in f]
-	for f in sv_files:
-		logging.info('Making plot for %s', f)
-		try:
-			sv = open_and_process_singular_values(f)
-			specs = f.strip('.npy')
-			plot_singular_values(sv, specs)
-		except:
-			with open(FAILED_FILES, 'a') as fail:
-				fail.write(f)
-			print('{} failed'.format(f))
+	layers = [f.split('_')[5] for f in sv_files]
+	by_layer = dict(zip(layers, [[f for f in sv_files if f.split('_')[5]==l] for l in layers]))
+	for layer in layers:sv_list = []
+		sv_list = []
+		specs = '_'.join(f.strip('.npy').split().pop(3))
+		for f in by_layer[layer]: #this area is broken -- need to only plot after the list is complete and naming needs to be fixed
+			logging.info('Making plot for %s', f)
+			try:
+				sv = open_and_process_singular_values(f)
+				sv_list.append(sv)
+			except:
+				with open(FAILED_FILES, 'a') as fail:
+					fail.write(f)
+				print('{} failed'.format(f))
+		plot_singular_values(sv_list, specs)
 
 

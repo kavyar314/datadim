3f683f5ca7e3bfe8909c3d978f4f36319b96528b
Kavya Ravichandran <rkavya@mit.edu>
2018-12-11 16:38:56 -0500

    calculate statistics for pairwise SVDs

---
+++ pairwise_stats.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from compute_stats import all_the_stats
+
+path_to_pairwise = '/mnt/datadim/datadim/pairwise_sv'
+outfile_path = './pairwise_statistics.csv'
+
+if __name__ == '__main__':
+	sv_files = [f for f in os.listdir(path_to_pairwise) if '.npy' in f]
+	for f in sv_files:
+		sv = np.load(f)
+		spec = f.split('_')
+		spec.pop()
+		spec = ''.join(spec).strip('.npy')
+		print spec
+		all_the_stats(sv, spec, outfile_path)

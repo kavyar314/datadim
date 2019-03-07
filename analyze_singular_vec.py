3fe5c17de0a229930f49fb1bbc38f553ca752269
Kavya Ravichandran <ravichandran.kavya@gmail.com>
2019-03-07 20:23:52 +0000

    paths corrected and formatting fixed

--- analyze_singular_vec.py
+++ analyze_singular_vec.py
@@ -4,8 +4,8 @@ import numpy as np
 class_list = range(10)
 layer_list = range(1,15)
 out_file = 'singular_vector_distances.csv'
-path_to_svec_one = ''
-path_to_svec_pair = ''
+path_to_svec_one = './singular_values_vecs/vgg/singularVectors_cifar10_train_c%d_activation_%d.npy'
+path_to_svec_pair = './pairwise_sv/vgg/singularVectors_cifar10_train_c%d_c%d_activation_%d.npy'
 
 def cosine_distance(v1, v2):
 	return np.dot(v1.T, v2)/(np.linalg.norm(v1)*np.linalg.norm(v2))
@@ -13,19 +13,20 @@ def cosine_distance(v1, v2):
 if __name__ == '__main__':
 	csv_out = "%d,%d,%d,%04f,%04f,%04f,%04f" # layer, class 1, class 2, (see notebook for order)
 	for layer in layer_list:
+                print("layer =", layer)
 		for i in range(len(class_list)):
 			c1 = class_list[i]
 			# load individual singular vector for c1
-			vecs_c1 = np.load(path_to_svec_one % c1)
+			vecs_c1 = np.load(path_to_svec_one % (c1,layer))
 			# get the first singular vector for c1
 			u_1_1 = vecs_c1[0,:]
 			for c2 in class_list[i+1:]:
 				# load individual singular vector for c2
-				vecs_c2 = np.load(path_to_svec_one % c2)
+				vecs_c2 = np.load(path_to_svec_one % (c2, layer))
 				# get the first singular vector for c2
 				u_2_1 = vecs_c2[0,:]
 				# load pair singular vector for c1, c2
-				vecs_pair = np.load(path_to_svec_pair % (c1, c2))
+				vecs_pair = np.load(path_to_svec_pair % (c1, c2, layer))
 				# get first and second vectors
 				u_12_1 = vecs_pair[0, :]
 				u_12_2 = vecs_pair[1, :]

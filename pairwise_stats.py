447ce8117184b488a2b86f773a2caeac84a34aca
Kavya Ravichandran <rkavya@mit.edu>
2019-04-15 14:22:38 -0400

    updating for current filestructure

--- pairwise_stats.py
+++ pairwise_stats.py
@@ -3,11 +3,11 @@ import os
 
 from compute_stats import all_the_stats
 
-path_to_pairwise = '/mnt/datadim/datadim/pairwise_sv'
-outfile_path = './pairwise_statistics.csv'
+path_to_pairwise = './pairwise_sv_centered'
+outfile_path = './pairwise_statistics_centered.csv'
 
 if __name__ == '__main__':
-	sv_files = [f for f in os.listdir(path_to_pairwise) if '.npy' in f]
+	sv_files = [f for f in os.listdir(path_to_pairwise) if '.npy' in f and 'singularValues' in f]
 	for f in sv_files:
 		sv = np.load(os.path.join(path_to_pairwise, f))
 		spec = f.split('_')

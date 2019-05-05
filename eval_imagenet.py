7e1b233ce815690c158a01dcbdb17a4071cf7dc5
Kavya Ravichandran <rkavya@mit.edu>
2019-05-05 18:00:35 -0400

    tabs

--- eval_imagenet.py
+++ eval_imagenet.py
@@ -11,9 +11,9 @@ correct = 0
 total = 0
 for k in classes:
 	print("predicting class k")
-    predicts = vgg.predict(train[k][0])
-    decode = decode_predictions(predicts)
-    top = [int(p[0][0].strip('n'))==k for p in decode]
-    correct += sum(top)
-    total += len(top)
+	predicts = vgg.predict(train[k][0])
+	decode = decode_predictions(predicts)
+	top = [int(p[0][0].strip('n'))==k for p in decode]
+	correct += sum(top)
+	total += len(top)
 print(correct/total)
